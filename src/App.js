f05d3ab04c604f62f9c7aa0cb61d2628d231454d
loryma <lorymalloc@gmail.com>
2019-12-06 09:31:13 +0200

    create store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import calendarReducer from "./store/reducers/calendarReducer";
+import eventsReducer from "./store/reducers/eventsReducer";
+import "./App.css";
+import CalendarContainer from "./components/CalendarContainer/CalendarContainer";
+
+const rootReducer = combineReducers({
+  calendar: calendarReducer,
+  events: eventsReducer
+});
+
+const store = createStore(rootReducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CalendarContainer />
+      </div>
+    </Provider>
   );
 }
 
